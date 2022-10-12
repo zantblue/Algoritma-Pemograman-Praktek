819d43192f4923f82e18932952981c97690ecb41
zantblue <96659260+zantblue@users.noreply.github.com>
2022-10-12 11:30:20 +0700

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -1,6 +1,32 @@
+##Challenge 1
+
 ##Challenge 2
+sisiAtas = input("masukan sisiAtas :")
+sisiBawah = input("masukan sisiBawah :")
+tinggi = input("masukan tinggi :")
+
+Luas = int(int(tinggi)*int(sisiAtas + sisiBawah)/2)
+print(int(Luas))
 
 ##Challenge 3
+nilai = input("masukkan nilai ")
+nilai = int(nilai)
+if nilai >100:
+    print("nilai yang dimasukkan 0-100")
+elif nilai >80:
+    print("nilai A")
+elif nilai >60:
+    print("nilai B")
+elif nilai >40:
+    print("nilai C")
+elif nilai >20:
+    print("nilai D")
+elif nilai >0:
+    print("nilai E")
+else:
+    print("Nilai yang dimasukkan 0-100")
+    
+##Challenge 4
 pembeli = input("Masukan jumlah belanja : ")
 pembeli = int(pembeli)
 if pembeli >= 100000:
