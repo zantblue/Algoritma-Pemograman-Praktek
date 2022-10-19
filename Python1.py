107397c6fe1dd8de8fd4e72e7201149e19f49dc7
zantblue <96659260+zantblue@users.noreply.github.com>
2022-10-19 10:59:19 +0700

    Create Python1.py

---
+++ Python1.py
@@ -0,0 +1,81 @@
+##Variabel = tempat untuk menyimpan data/nilai 
+
+##penullisan variabel
+from code import InteractiveInterpreter
+
+
+nama = "Muh Ryamizard Albasith"
+Nama = "Mizard"
+
+##Batasan penulisan variabel
+
+##1. Angka tidak bisa ditulis di depan
+##contoh : 1nama 
+nama1 = "Mizard" ##bisa
+
+##Penulisan variabel 2 (Camel Case)
+namaLengkap = "Muh Ryamizard"
+namaLengkap = "Mizard"
+namaLengkapSaya = "Jamal Udin"
+NamaLengkapSaya = "Udin Ngab"
+
+##Penulisan variabel 3 (Snake Case)
+nama_lengkap = "Muh Ryamizard"
+nama_Lengkap = "Mizard"
+nama_lengkap_saya = "Albasith"
+
+
+
+##Tipe Data = Jenis nilai atau jenis datanya
+##Tipe Data 'string'
+alamat = "Yogyakarta"
+asalKota = 'Probolinggo'
+umur = "1"
+
+##Tipe Number 
+##Tipe data 'interger'(bilangan bulat)
+umurSaya = 10
+nilaiSaya = 100
+Urutan = 1
+negatif = -100
+##Tipe data float (desimal)
+beratBadan = 70.1 
+tinggiBadan = 100.1000 
+##Tipe dat 'Boolean'
+tampan = True ##Bisa bernilai 1
+jelek = False ##Bisa bernilai 0
+
+##Operator aritmatika
+#1. Penjumlahan 
+angka1 = 10
+angka2 = 20
+jumlah = angka1 + angka2
+print("jumlah",jumlah)
+#2. Pengurangan 
+angka1 = 20
+angka2 = 10
+kurang = angka1 - angka2
+print("kurang",kurang)
+##3. Perkalian
+angka1 = 10
+angka2 = 10
+kali = angka1 * angka2
+print("perkalian",kali)
+##4. Pembagian
+angka1 = 100
+angka2 = 10
+bagi = angka1 / angka2
+print("bagi",bagi)
+
+#user = int (input("Masukkan panjang : "))
+#jumlah = user + user 
+#print(Jumlah)
+
+tinggi = 10
+alas = 5
+Luas = alas * tinggi 
+
+tinggi = int (input("tinggi : "))
+alas = int (input("alas : "))
+Luas = tinggi * alas /2
+print(Luas)
