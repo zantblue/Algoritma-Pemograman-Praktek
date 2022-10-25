147e698b6bb7782e14cc49483cdf537c790db7c0
zantblue <96659260+zantblue@users.noreply.github.com>
2022-10-25 08:05:00 +0700

    Menambahkan penulisan data

--- Python6.py
+++ Python6.py
@@ -1,1 +1,24 @@
-tes()
+##penulisan List
+
+##Indexing 
+#pemanggilan data bisa menggunakan angka
+#         0/-4    1/-3   2/-2   3/-1  
+data = ["mizard","udin","ngab","jamal"]
+print(data[1])
+
+#Manipulasi list
+#Insert (Menambahkan nilai berdasarkan index)
+# data.insert(1,"asep") #Index, nilainnya
+# print(data)
+# data.insert(1,"budi")
+# print(data)
+
+#Append (Menambahkan nilai pada akhir list)
+# data.append("budi")
+# data.append("yusup")
+# data.append("rido")
+# print(data)
+
+#Extend (Menambahkan list di dalam list)
+# data_baru = [10,20,30,True]
+# data.extend(data_baru)
