fb8045285bae9a11a3ba3b206d38e023c0d680a0
zantblue <96659260+zantblue@users.noreply.github.com>
2022-10-26 11:29:45 +0700

    menambahkan Slicing

--- Python6.py
+++ Python6.py
@@ -37,6 +37,16 @@ dataAngka = [10,20,40,50,60,30]
 #Next : Slicing
 dataAngka.sort()
 print(dataAngka)
+
 #Reverse (Mengurutkan dari yang terbesar ke yg terkecil)
 dataAngka.reverse()
 print(dataAngka)
+
+# Slicing (pemotongan)
+data = ["mizard","jamal","udin","ngab"]
+dataAngka = [10,20,30,40,45]
+print(data[1]) Mengambil data list berdasarkan index
+SlicingData = data[1:2] #index awal : index setelahnya
+SlicingDataAwalSampaiAkhir = data[1:] #Data awal sampai data akhir
+SlicingBatasDataAkhir = data[:3] #kasih data sampai akhir
+print(data[:3])
