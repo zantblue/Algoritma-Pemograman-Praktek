c019580ce2897041ebe498c4a179e4468e60a64e
zantblue <96659260+zantblue@users.noreply.github.com>
2022-10-26 12:03:52 +0700

    menambah challenge6

--- Challenge.py
+++ Challenge.py
@@ -81,3 +81,20 @@ elif pilih == 2:
     luasLingkaran()
 elif pilih == 3:
     luasPersegi()
+    
+#Challenge 6
+    #mencari data berdasarkan nim, nim berdasarkan imputan dari user
+dataNIM = int(input("Masukan NIM yang anda cari :"))
+
+data = [
+        [500411291, "Faiz"],
+        [500411292, "Nazhir"],
+        [500411293, "Amrulloh"],
+        [500411294, "Reza"],
+        [500411295, "Saputra"]
+]
+        
+for i in data:
+    if i[0] == dataNIM:
+        print(i)
+        break
