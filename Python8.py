67c8b5317ee7edae4a2e8e4452d047683985b0a8
zantblue <96659260+zantblue@users.noreply.github.com>
2022-11-30 12:18:53 +0700

    Create Python8.py

---
+++ Python8.py
@@ -0,0 +1,17 @@
+#Pengenalan Library
+    #Membuat virtual environment
+    #Numpy (Numerical Python)*
+    #Matplotlib
+    
+import numpy as np
+import matplotlib.pyplot as plt
+
+# datalist = [1,2,3,4,5] + [6,7,8,9,10]
+# datanumpy = np.array([1,2,3,4,5] + [6,7,8,9,10])
+# print(datalist)
+# print(datanumpy)
+
+data = [0,60,60,60,60,0] + [0,60,60,60,60,0]
+plt.plot(data)
+plt.show()
+
