f9e547533c972ae61d04723f37b30616130deab0
zantblue <96659260+zantblue@users.noreply.github.com>
2022-12-20 08:40:18 +0700

    nambahin challenge11

--- Challenge.py
+++ Challenge.py
@@ -1,4 +1,4 @@
-##Challenge 1
+###Challenge 1
 tinggi = 10
 alas = 5
 Luas = alas * tinggi 
@@ -8,7 +8,7 @@ alas = int (input("alas : "))
 Luas = tinggi * alas /2
 print("Luas",Luas)
 
-##Challenge 2
+###Challenge 2
 sisiAtas = input("masukan sisiAtas :")
 sisiBawah = input("masukan sisiBawah :")
 tinggi = input("masukan tinggi :")
@@ -16,7 +16,7 @@ tinggi = input("masukan tinggi :")
 Luas = int(int(tinggi)*int(sisiAtas + sisiBawah)/2)
 print(int(Luas))
 
-##Challenge 3
+###Challenge 3
 nilai = input("masukkan nilai ")
 nilai = int(nilai)
 if nilai >100:
@@ -34,7 +34,7 @@ elif nilai >0:
 else:
     print("Nilai yang dimasukkan 0-100")
     
-##Challenge 4
+###Challenge 4
 pembeli = input("Masukan jumlah belanja : ")
 pembeli = int(pembeli)
 if pembeli >= 100000:
@@ -46,7 +46,7 @@ total = pembeli - hitung
 print("diskon :",hitung)
 print("harga setelah diskon :", total)
 
-##Challenge 5
+###Challenge 5
 for i in range(2,41):
     if i == 10:
         print("ini adalah nilai",i)
@@ -58,7 +58,7 @@ for i in range(2,41):
         print("ini adalah nilai",i)
         break
         
-#Challenge 6
+###Challenge 6
 def luasSegitiga():
     alas = int(input("Masukkan panjang alas = "))
     tinggi = int(input("Masukkan panjang tinggi = "))
@@ -83,7 +83,7 @@ elif pilih == 2:
 elif pilih == 3:
     luasPersegi()
 
-#Challenge 9
+###Challenge 9
 import numpy as np
 
 data1 = (10,'mizard',20,30,40)
@@ -102,7 +102,7 @@ print("Nama :",data1[1])
 print("NIM :",data2[0])
 print("Result :",hasil)
 
-##Challenge10
+###Challenge10
 import numpy as np
 
 data1 = (1,2,3,4,'mizard',"5220411093",5,6,7,8,9,10)
@@ -128,3 +128,83 @@ print("NIM :",data2[5])
 print("umur :",data2[1])
 print(len(genap))
 print(len(ganjil))
+
+###Challenge11
+import mysql.connector
+import matplotlib.pyplot as plt
+
+db = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "",
+    database = "db_mahasiswa",
+)
+myCursor = db.cursor()
+# sql = "SELECT * FROM tb_data"
+# myCursor.execute(sql)
+# result = myCursor.fetchall()
+data = []
+def mahasiswa():
+    Nama = input("masukan Nama Anda : ")
+    NIM = input("masukan NPM : ")
+    data.append(Nama)
+    data.append(NIM)
+    sql = "INSERT INTO tb_data (Nama, NIM) VALUES (%s,%s)"
+    myCursor.execute(sql, data)
+    db.commit()
+
+def tampil():
+    sql = "SELECT * FROM tb_data"
+    myCursor.execute(sql)
+    result = myCursor.fetchall()
+    for i in result:
+        print(i)
+
+def TampilNamaNim():
+    sql = "SELECT * FROM tb_data"
+    myCursor.execute(sql)
+    result = myCursor.fetchall()
+    for i in result:
+        print(i[1],',',i[2])
+
+def cari():
+    sql = "SELECT * FROM tb_data"
+    myCursor.execute(sql)
+    cari = input("Masukan NIM : ")
+    result = myCursor.fetchall()
+    for i in result:
+        if i[2]== cari:
+            print(i)
+
+def graph():
+    sql = "SELECT * FROM tb_nilai"
+    myCursor.execute(sql)
+    result = myCursor.fetchone()
+    nilai = ["agama","bahasa","alpro"]
+    print(result)
+    plt.bar(nilai,result[1:])
+    plt.show()
+
+
+while True:
+    print("========MENU========")
+    print("1. Input data")
+    print("2. Tampilkan semua data")
+    print("3. Tampilkan Nama, NIM")
+    print("4. Cari data")
+    print("5. tampilkan Grafik")
+    print("6. Exit")
+    pilihan = input("Pilih Menu Yang Tersedia : ")
+    print("========================")
+    if pilihan == "1":
+        mahasiswa()
+    elif pilihan =="2":
+        tampil()
+    elif pilihan =="3":
+        TampilNamaNim()
+    elif pilihan =="4":
+        cari()
+    elif pilihan =="5":
+        graph()
+    elif pilihan =="6":
+        exit()
